5fb0e2d060dcbfc1ebfbd324f5c5d3b9e85bec8f
claytonflesher <claytonflesher@gmail.com>
2018-12-01 23:25:50 -0600

    Working solution for both day 1 problems

--- 2018/day_1/frequency.exs
+++ 2018/day_1/frequency.exs
@@ -3,13 +3,36 @@ defmodule AdventOfCode.Frequency do
     change_frequency(list, 0)
   end
 
+  def calculate_first_duplicate(list) do
+    calculate_first_duplicate(list, %{0 => 1}, 0, list)
+  end
+
   defp change_frequency([offset | tail], state) do
-    new_state = state + String.to_integer(offset)
+    new_state = state + offset
     change_frequency(tail, new_state)
   end
 
   defp change_frequency([], state) do
     state
+  end
+
+  defp calculate_first_duplicate([head | tail], previous, frequency, list) do
+    new_state = frequency + head
+    case Map.has_key?(previous, new_state) do
+      true ->
+        new_state
+      false ->
+        calculate_first_duplicate(
+          tail,
+          Map.put(previous, new_state, 1),
+          new_state,
+          list
+        )
+    end
+  end
+
+  defp calculate_first_duplicate([], previous, frequency, list) do
+    calculate_first_duplicate(list, previous, frequency, list)
   end
 end
 
@@ -17,7 +40,9 @@ end
 file
 |> String.split("\n")
 |> List.delete_at(-1)
-|> AdventOfCode.Frequency.calculate_frequency
+|> Enum.map(fn (item) -> String.to_integer(item) end)
+#|> AdventOfCode.Frequency.calculate_frequency
+|> AdventOfCode.Frequency.calculate_first_duplicate
 |> IO.inspect
 
 
